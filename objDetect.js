c8050bed85686d198b6cb1adb4316891d0a88e84
cgarciaflor <cristiangarciaflor@gmail.com>
2024-05-03 16:50:16 -0400

    more updates

---
+++ objDetect.js
@@ -0,0 +1,44 @@
+function previewImage() {
+    var fileInput = document.getElementById('fileInput');
+    var file = fileInput.files[0];
+    
+    var imagePreview = document.getElementById('imagePreview');
+    imagePreview.innerHTML = ''; // Clear previous image preview
+    
+    var img = document.createElement('img');
+    img.src = URL.createObjectURL(file);
+    img.style.maxWidth = '100%'; // Set maximum width for the image
+    imagePreview.appendChild(img);
+}
+
+function detectObjects() {
+    var fileInput = document.getElementById('fileInput');
+    var file = fileInput.files[0];
+    var form = new FormData();
+    form.append("imageFile", file, "file");
+
+    var settings = {
+        "url": "https://api.cloudmersive.com/image/recognize/detect-objects",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Apikey": "f647df10-dfea-4c71-97d3-7e95052cf017"
+        },
+        "processData": false,
+        "mimeType": "multipart/form-data",
+        "contentType": false,
+        "data": form
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response); // Print the JSON response to the console
+        displayResults(response);
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log("Error:", textStatus, errorThrown);
+    });
+}
+
+function displayResults(response) {
+    var resultDiv = document.getElementById('result');
+    resultDiv.textContent = JSON.stringify(response, null, 2); // Print entire JSON response
+}
